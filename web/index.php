30e4d31ada3241ade7ecec00883fb1a800920cf8
Tom Silver <tomlikefuturam@gmail.com>
2014-05-09 23:41:56 +0800

    updated

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ require_once (__DIR__ . "/../vendor/autoload.php");
 
 $app = new Silex\Application();
 
-$app->get ("/blog", function () {
+$app->get ("/gajeweb/blog", function () {
 
     echo "hello world";
     exit;
