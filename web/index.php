a72248e4cde303a895f392137d0d1af3d6c005e8
Tom Silver <tomlikefuturam@gmail.com>
2014-05-09 23:18:18 +0800

    changed folder

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ require_once (__DIR__ . "/../vendor/autoload.php");
 
 $app = new Silex\Application();
 
-$app->get ("/gaje/blog", function () {
+$app->get ("/blog", function () {
 
     echo "hello world";
     exit;
