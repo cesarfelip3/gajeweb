bb4ba12943e143080f7cc2b5ff7bca0c9960ca91
Tom Silver <tomlikefuturam@gmail.com>
2014-05-10 03:02:41 +0800

    update

--- web/index.php
+++ web/index.php
@@ -13,26 +13,47 @@ require_once (__DIR__ . "/config.php");
 
 $app = new Silex\Application();
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$app->error(function (\Exception $e, $code) {
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+    return new Response($message);
+});
+
 $app["debug"] = true;
+$app['asset.host'] = 'http://localhost/image/';
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), $dbconfig);
 
 $basename = "/gajeweb";
+$api_v1 = "api/v1/";
 
-$id = 0;
+$id= 'id';
 $router_api = array (
-    "blog/{id}" => 0,
+    $api_v1 . "image/upload/{{$id}}" => $id,
 );
 
-foreach ($router_api as $router => $parameter) {
-    $app->get ($basename . "/" . $router, function ($id) use ($app) {
+foreach ($router_api as $router => $id) {
+    $app->post ($basename . "/" . $router, function ($id) use ($app) {
+
 
         $sql = "SELECT * FROM image WHERE image_id = ?";
         $post = $app['db']->fetchAssoc($sql, array((int) 0));
 
         return "hello router = " . $id;
 
-    });
+    }
 }
+
+$app->before(function (Request $request) {
+    // ...
+});
 
 $app->run();
 
