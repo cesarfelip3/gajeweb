812c769ce44053d3081b77da25df36e4b3e9b6d6
hello <hello@hellotekiMac-mini.local>
2014-07-12 14:53:08 +0800

    update

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -126,6 +126,22 @@ class Api
             return $app->json($controller->getError(), $status);
 
         });
+
+        $api->post("user/follower/list", function (Request $request) use ($app) {
+
+            $controller = new Controller\UserController($request, $app);
+            $ret = $controller->getFollowerListByUser();
+
+            $status = 200;
+            if ($ret) {
+                $status = 200;
+            } else {
+                $status = 400;
+            }
+
+            return $app->json($controller->getError(), $status);
+
+        });
     }
 
     protected function _imageAPI (&$api)
