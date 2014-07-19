2d333ab4cd8b9d56e7fcba74dae3309d840f075b
hello <hello@hellotekiMac-mini.local>
2014-07-19 13:03:41 +0800

    update

---
+++ src/Api/Controller/NoteController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Api\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+use Api\Controller\BaseController;
+use Model\Image;
+use Model\User;
+use Model\Note;
+
+class NoteController extends BaseController
+{
+
+    public function __construct($request, $app)
+    {
+        $this->app = $app;
+        $this->request = $request;
+        $this->response = new Response();
+    }
+
+    public function getNoteList()
+    {
+        $page = $this->request->get("page", 0);
+        $pageSize = $this->request->get("page_size", 1);
+
+        $data = array();
+        $data["page"] = intval($page);
+        $data["page_size"] = intval($pageSize);
+
+        $note = new Note();
+        $result = $note->getNoteList($data);
+
+        if (empty ($result)) {
+
+            $this->setSuccess("empty result from db", array("notes" => array()));
+        } else {
+
+            $this->setSuccess("", array("notes" => $result));
+        }
+
+        return true;
+
+    }
+}
