4495e1e342521320dbf8315b7cd66009eaf0082e
hello <hello@hellotekiMac-mini.local>
2014-07-22 20:52:20 +0800

    update

--- src/Model/User.php
+++ src/Model/User.php
@@ -90,6 +90,14 @@ class User extends BaseModel
     public function addFollow ($data)
     {
         $data["create_date"] = time();
+
+        if ($this->followExist(array (
+            "user_followed_uuid" => $data["user_following_uuid"],
+            "user_following_uuid" => $data["user_followed_uuid"]
+        ))) {
+            $data["is_mutual"] = 1;
+        }
+
         $this->db->insert($this->table_user_follow, $data);
     }
 
@@ -122,7 +130,7 @@ class User extends BaseModel
 
         $limit = "$page, $pageSize";
 
-        $sql = "SELECT DISTINCT usr.* FROM {$this->table} usr INNER JOIN {$this->table_user_follow} fol ON usr.user_uuid=fol.user_followed_uuid ORDER BY fol.create_date DESC LIMIT {$limit}";
+        $sql = "SELECT DISTINCT usr.*, fol.is_mutual AS is_mutual FROM {$this->table} usr INNER JOIN {$this->table_user_follow} fol ON usr.user_uuid=fol.user_followed_uuid ORDER BY fol.create_date DESC LIMIT {$limit}";
         $stmt = $this->db->prepare($sql);
         //$stmt->bindValue (1, $data["user_uuid"]);
         $stmt->execute();
