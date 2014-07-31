d63635c9fa522c332ecdfeb72b91a3580532d4e4
hello <hello@hellotekiMac-mini.local>
2014-07-31 21:51:45 +0800

    update

--- src/Api/Controller/ImageController.php
+++ src/Api/Controller/ImageController.php
@@ -91,6 +91,8 @@ class ImageController extends BaseController
             return $this->setFailed("file handler not exist");
         }
 
+        return $this->setFailed("", array((array)$file));
+
         if ($file->isValid()) {
 
             $data = array();
