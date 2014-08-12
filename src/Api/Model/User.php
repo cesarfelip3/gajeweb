42e87a1b55b8e194a65cebd85fd4331e28188c4d
hello <hello@hellotekiMac-mini.local>
2014-08-12 20:01:13 +0800

    update

--- src/Api/Model/User.php
+++ src/Api/Model/User.php
@@ -205,6 +205,31 @@ class User extends BaseModel
         return $result;
     }
 
+    //====================================
+    //
+    //====================================
+
+    public function getUpdateInfo ()
+    {
+        $page = $data["page"];
+        $pageSize = $data["page_size"];
+
+        $page = intval($page);
+        $pageSize = intval($pageSize);
+        $page = $page * $pageSize;
+
+        $limit = "$page, $pageSize";
+
+        $sql = "SELECT DISTINCT usr.*, fol.is_mutual AS is_mutual FROM {$this->table} usr INNER JOIN {$this->table_user_follow} fol ON usr.user_uuid=fol.user_followed_uuid WHERE fol.user_following_uuid=? ORDER BY fol.create_date DESC LIMIT {$limit}";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue (1, $data["user_uuid"]);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+
     public static function table()
     {
         return "user";
