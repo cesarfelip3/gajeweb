c0e6ccb6920afe36e1b7a69c7455ef8d691f026d
hello <hello@hellotekiMac-mini.local>
2014-08-12 17:20:35 +0800

    update

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -333,7 +333,7 @@ class Api
 
         $api->before(function (Request $request, $app) use ($config) {
 
-            return null;
+            //return null;
 
             $token = $request->headers->get("X-AUTH-KEY");
 
