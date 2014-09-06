ed6254ec1416a14f071fec1868f27dac2086869a
hello <hello@hellodeMac-mini.local>
2014-09-06 21:56:30 +0800

    update

--- data/database.sql
+++ data/database.sql
@@ -28,6 +28,23 @@ DELETE FROM `administrator`;
 /*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
 
 
+-- 导出  表 gajeapp.cache 结构
+DROP TABLE IF EXISTS `cache`;
+CREATE TABLE IF NOT EXISTS `cache` (
+  `cache_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(10) unsigned NOT NULL,
+  `key` char(32) NOT NULL,
+  `content` text NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`cache_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 正在导出表  gajeapp.cache 的数据：~0 rows (大约)
+DELETE FROM `cache`;
+/*!40000 ALTER TABLE `cache` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache` ENABLE KEYS */;
+
+
 -- 导出  表 gajeapp.comment 结构
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
@@ -107,33 +124,34 @@ DELETE FROM `image_comment`;
 /*!40000 ALTER TABLE `image_comment` ENABLE KEYS */;
 
 
+-- 导出  表 gajeapp.note 结构
+DROP TABLE IF EXISTS `note`;
+CREATE TABLE IF NOT EXISTS `note` (
+  `note_id` int(20) NOT NULL AUTO_INCREMENT,
+  `note_uuid` char(50) NOT NULL DEFAULT '',
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `create_date` int(11) NOT NULL DEFAULT '0',
+  `modified_date` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`note_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 正在导出表  gajeapp.note 的数据：~0 rows (大约)
+DELETE FROM `note`;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+
+
 -- 导出  表 gajeapp.theme 结构
 DROP TABLE IF EXISTS `theme`;
 CREATE TABLE IF NOT EXISTS `theme` (
   `theme_id` int(20) NOT NULL AUTO_INCREMENT,
   `theme_uuid` char(50) NOT NULL DEFAULT '',
   `name` varchar(50) NOT NULL DEFAULT '',
-  `description` text DEFAULT '',
+  `description` text,
   `create_date` int(11) NOT NULL DEFAULT '0',
   `modified_date` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`theme_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 正在导出表  gajeapp.theme 的数据：~0 rows (大约)
-DELETE FROM `theme`;
-/*!40000 ALTER TABLE `theme` DISABLE KEYS */;
-/*!40000 ALTER TABLE `theme` ENABLE KEYS */;
-
--- 导出  表 gajeapp.theme 结构
-DROP TABLE IF EXISTS `note`;
-CREATE TABLE IF NOT EXISTS `note` (
-  `note_id` int(20) NOT NULL AUTO_INCREMENT,
-  `note_uuid` char(50) NOT NULL DEFAULT '',
-  `name` varchar(50) NOT NULL DEFAULT '',
-  `description` text NOT NULL DEFAULT '',
-  `create_date` int(11) NOT NULL DEFAULT '0',
-  `modified_date` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`note_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 正在导出表  gajeapp.theme 的数据：~0 rows (大约)
@@ -178,6 +196,22 @@ DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 
+-- 导出  表 gajeapp.user_block 结构
+DROP TABLE IF EXISTS `user_block`;
+CREATE TABLE IF NOT EXISTS `user_block` (
+  `user_block_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_uuid` char(50) NOT NULL,
+  `user_block_uuid` char(50) NOT NULL,
+  `create_date` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_block_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 正在导出表  gajeapp.user_block 的数据：~0 rows (大约)
+DELETE FROM `user_block`;
+/*!40000 ALTER TABLE `user_block` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_block` ENABLE KEYS */;
+
+
 -- 导出  表 gajeapp.user_follow 结构
 DROP TABLE IF EXISTS `user_follow`;
 CREATE TABLE IF NOT EXISTS `user_follow` (
@@ -193,19 +227,213 @@ CREATE TABLE IF NOT EXISTS `user_follow` (
 DELETE FROM `user_follow`;
 /*!40000 ALTER TABLE `user_follow` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_follow` ENABLE KEYS */;
+
+
+-- 导出  视图 gajeapp.view_image_brander_collection 结构
+DROP VIEW IF EXISTS `view_image_brander_collection`;
+-- 创建临时表以解决视图依赖性错误
+CREATE TABLE `view_image_brander_collection` (
+	`user_id` INT(11) NOT NULL,
+	`user_uuid` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_token` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_icon` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`password` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`firstname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`lastname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`fullname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`location` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`longtitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`latitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`city` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`state` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`country` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`phone` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`image_latest` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`images` INT(20) NOT NULL,
+	`create_date` INT(20) NOT NULL,
+	`modified_date` INT(20) NOT NULL,
+	`comment_id` INT(20) NOT NULL,
+	`brander_id` INT(20) NOT NULL,
+	`follower_id` INT(20) NOT NULL,
+	`date_added` INT(20) NOT NULL
+) ENGINE=MyISAM;
+
+
+-- 导出  视图 gajeapp.view_image_comment_collection 结构
+DROP VIEW IF EXISTS `view_image_comment_collection`;
+-- 创建临时表以解决视图依赖性错误
+CREATE TABLE `view_image_comment_collection` (
+	`comment_id` INT(20) NOT NULL,
+	`comment_uuid` CHAR(128) NULL COLLATE 'utf8_general_ci',
+	`user_uuid` CHAR(128) NULL COLLATE 'utf8_general_ci',
+	`content` VARCHAR(512) NULL COLLATE 'utf8_general_ci',
+	`create_date` INT(20) NULL,
+	`modified_date` INT(20) NULL,
+	`user_token` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`fullname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci'
+) ENGINE=MyISAM;
+
+
+-- 导出  视图 gajeapp.view_image_latest_collection 结构
+DROP VIEW IF EXISTS `view_image_latest_collection`;
+-- 创建临时表以解决视图依赖性错误
+CREATE TABLE `view_image_latest_collection` (
+	`image_uuid` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`name` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`description` VARCHAR(512) NOT NULL COLLATE 'utf8_general_ci',
+	`width` INT(11) NOT NULL,
+	`height` INT(11) NOT NULL,
+	`create_date` INT(11) NOT NULL,
+	`modified_date` INT(11) NOT NULL,
+	`file_name` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`thumbnail` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`user_uuid` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`user_token` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`fullname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci'
+) ENGINE=MyISAM;
+
+
+-- 导出  视图 gajeapp.view_user_follower_collection 结构
+DROP VIEW IF EXISTS `view_user_follower_collection`;
+-- 创建临时表以解决视图依赖性错误
+CREATE TABLE `view_user_follower_collection` (
+	`user_id` INT(11) NOT NULL,
+	`user_uuid` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_token` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_icon` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`password` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`firstname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`lastname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`fullname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`location` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`longtitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`latitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`city` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`state` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`country` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`phone` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`image_latest` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`images` INT(20) NOT NULL,
+	`create_date` INT(20) NOT NULL,
+	`modified_date` INT(20) NOT NULL,
+	`comment_id` INT(20) NOT NULL,
+	`brander_id` INT(20) NOT NULL,
+	`follower_id` INT(20) NOT NULL,
+	`is_mutual` INT(20) NOT NULL
+) ENGINE=MyISAM;
+
+
+-- 导出  视图 gajeapp.view_user_following_collection 结构
+DROP VIEW IF EXISTS `view_user_following_collection`;
+-- 创建临时表以解决视图依赖性错误
+CREATE TABLE `view_user_following_collection` (
+	`user_id` INT(11) NOT NULL,
+	`user_uuid` CHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_token` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`facebook_icon` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`username` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci',
+	`email` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`password` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`firstname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`lastname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`fullname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`location` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`longtitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`latitude` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`city` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`state` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`country` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`phone` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`image_latest` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`images` INT(20) NOT NULL,
+	`create_date` INT(20) NOT NULL,
+	`modified_date` INT(20) NOT NULL,
+	`comment_id` INT(20) NOT NULL,
+	`brander_id` INT(20) NOT NULL,
+	`follower_id` INT(20) NOT NULL,
+	`is_mutual` INT(20) NOT NULL
+) ENGINE=MyISAM;
+
+
+-- 导出  视图 gajeapp.view_image_brander_collection 结构
+DROP VIEW IF EXISTS `view_image_brander_collection`;
+-- 移除临时表并创建最终视图结构
+DROP TABLE IF EXISTS `view_image_brander_collection`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `view_image_brander_collection` AS SELECT 
+	usr.*, 
+	imgb.create_date AS date_added 
+FROM image_brander imgb INNER JOIN user usr 
+ON imgb.user_uuid=usr.user_uuid 
+ORDER BY imgb.create_date ASC ;
+
+
+-- 导出  视图 gajeapp.view_image_comment_collection 结构
+DROP VIEW IF EXISTS `view_image_comment_collection`;
+-- 移除临时表并创建最终视图结构
+DROP TABLE IF EXISTS `view_image_comment_collection`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `view_image_comment_collection` AS SELECT 
+	cmt.*,
+	usr.facebook_token AS user_token, 
+	usr.fullname AS fullname, 
+	usr.username AS username	
+FROM image_comment imgc INNER JOIN comment cmt 
+ON imgc.comment_uuid=cmt.comment_uuid 
+INNER JOIN user usr 
+ON cmt.user_uuid=usr.user_uuid 
+ORDER BY cmt.create_date ASC ;
+
+
+-- 导出  视图 gajeapp.view_image_latest_collection 结构
+DROP VIEW IF EXISTS `view_image_latest_collection`;
+-- 移除临时表并创建最终视图结构
+DROP TABLE IF EXISTS `view_image_latest_collection`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `view_image_latest_collection` AS SELECT 
+	img.image_uuid, 
+	img.name, 
+	img.description, 
+	img.width, 
+	img.height, 
+	img.create_date, 
+	img.modified_date, 
+	img.file_name, 
+	img.thumbnail,
+	img.user_uuid AS user_uuid, 
+	usr.facebook_token AS user_token,
+	usr.fullname AS fullname, 
+	usr.username AS username 
+FROM image img INNER JOIN user usr 
+ON img.user_uuid=usr.user_uuid 
+ORDER BY img.modified_date ;
+
+
+-- 导出  视图 gajeapp.view_user_follower_collection 结构
+DROP VIEW IF EXISTS `view_user_follower_collection`;
+-- 移除临时表并创建最终视图结构
+DROP TABLE IF EXISTS `view_user_follower_collection`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `view_user_follower_collection` AS SELECT DISTINCT 
+	usr.*, 
+	fol.is_mutual AS is_mutual 
+FROM user usr INNER JOIN user_follow fol 
+ON usr.user_uuid=fol.user_following_uuid 
+ORDER BY fol.create_date DESC ;
+
+
+-- 导出  视图 gajeapp.view_user_following_collection 结构
+DROP VIEW IF EXISTS `view_user_following_collection`;
+-- 移除临时表并创建最终视图结构
+DROP TABLE IF EXISTS `view_user_following_collection`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `view_user_following_collection` AS SELECT DISTINCT 
+	usr.*, 
+	fol.is_mutual AS is_mutual 
+FROM user usr INNER JOIN user_follow fol 
+ON usr.user_uuid=fol.user_followed_uuid 
+ORDER BY fol.create_date DESC ;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
--- 导出  表 gajeapp.user_follow 结构
-DROP TABLE IF EXISTS `user_block`;
-CREATE TABLE IF NOT EXISTS `user_block` (
-  `user_block_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_uuid` char(50) NOT NULL,
-  `user_block_uuid` char(50) NOT NULL,
-  `create_date` int(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_block_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 正在导出表  gajeapp.user_follow 的数据：~0 rows (大约)
-DELETE FROM `user_block`;
