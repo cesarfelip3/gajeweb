c13346d2d9f741aa89e5ab2556a32579d6b6f446
hello <hello@hellodeMacBook-Air.local>
2014-12-11 08:56:22 +0800

    update

--- src/Api/Model/Image.php
+++ src/Api/Model/Image.php
@@ -312,6 +312,8 @@ class Image extends BaseModel
         $total = $this->getTotalInTheme($data);
 
         print_r ($total);
+        ceil ($total / 25);
+        ceil (121 / 25);
         exit;
 
         for ($i = 0; $i < ceil ($total / 25); ++$i) {
